7e8535b42909427a930db1d39b78dc0c97fc12f5
Ethan Barron <83491820+EthanJamesBarron@users.noreply.github.com>
2025-08-21 12:43:03 +0100

    Initial setup + assembly publicizing

--- TownshipLib/Main.cs
+++ TownshipLib/Main.cs
@@ -4,14 +4,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(TownshipLib.Main), "TownshipLib", "0.0.1", "Ethan Barron")]
 
 namespace TownshipLib
 {
     public class Main : MelonMod
     {
+        private bool headless;
+        public static MelonLogger.Instance Logger;
+
+
+        public override void OnInitializeMelon()
+        {
+            Logger = LoggerInstance;
+        }
+
         public override void OnUpdate()
         {
             
+        }
+
+        public override void OnGUI()
+        {
+            headless = GUILayout.Toggle(headless, "Headless?");
+
+            if (GUILayout.Button("Start Dedicated Server"))
+            {
+                ApplicationStartupManager.AttemptServerStartAsync(new string[]
+                {
+                    "/start_server", "-1", "true", "5999"
+                });
+            }
         }
     }
 }
