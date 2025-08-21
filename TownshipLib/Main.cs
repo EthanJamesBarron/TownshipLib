c990036fb95d9e25dfbc6f37674d0d97420d75ae
Ethan Barron <83491820+EthanJamesBarron@users.noreply.github.com>
2025-08-21 12:15:01 +0100

    Add project files.

---
+++ TownshipLib/Main.cs
@@ -0,0 +1,17 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TownshipLib
+{
+    public class Main : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            
+        }
+    }
+}
