59a048c78c0ef6a724066b9612e12a1301bd77b6
Ethan Barron <83491820+EthanJamesBarron@users.noreply.github.com>
2025-08-23 10:08:09 +0100

    API redirector in place for migration away from Alta

--- TownshipLib/APIMigrator.cs
+++ TownshipLib/APIMigrator.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Alta.Api.Client.Utility;
+using Alta.Api.DataTransferModels.Extensions;
 using HarmonyLib;
+using MelonLoader.Logging;
 
 namespace TownshipLib
 {
@@ -15,16 +17,6 @@ namespace TownshipLib
         public static bool APIEndpointRedirect()
         {
             ApiAccess.StartWithEndpoint("http://147.185.221.31:17891/");
-            return false;
-        }
-
-        [HarmonyPatch(typeof(LoginUtilities), nameof(LoginUtilities.HashString)), HarmonyPrefix]
-        public static bool AntiPasswordHasher(string text, ref string __result)
-        {
-            // Work on this later, for now just disable hashing entirely
-            Main.Logger.Warning("The provided text was not hashed and will be returned as-is, This may expose sensitive information");
-            
-            __result = text;
             return false;
         }
     }
